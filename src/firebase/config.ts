1658cd54f1760ea4312beffd94f3f7ba77905c1b
Chintamani Tare <chintamani1804@gmail.com>
2025-12-27 07:01:46 +0000

    Firebase: Error (auth/invalid-api-key).

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9999477035-9d719",
   "appId": "1:883899499858:web:1587d9e3bd40819b00668b",
-  "apiKey": "AIzaSyAtKQt1JGym3B56oRLoUSY5FHv_RNh1YbE",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-9999477035-9d719.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "883899499858"
