98885a40cba1d0cebea3ebf24cef01b7b7b76fd6
Chintamani Tare <chintamani1804@gmail.com>
2025-12-27 06:26:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9999477035-9d719",
+  "appId": "1:883899499858:web:1587d9e3bd40819b00668b",
+  "apiKey": "AIzaSyAtKQt1JGym3B56oRLoUSY5FHv_RNh1YbE",
+  "authDomain": "studio-9999477035-9d719.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "883899499858"
+};
